f38d3a041e50240a7122767c5eb974652f59c8b6
Bantty Kumar <kbunty28@gmail.com>
2022-08-25 13:26:41 +0530

    intial commit

---
+++ src/main/java/com/bktech/controller/EmployeeController.java
@@ -0,0 +1,16 @@
+package com.bktech.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/emp")
+public class EmployeeController {
+	
+	@GetMapping("/msg")
+	public ResponseEntity<String> getMsg(){
+		return ResponseEntity.ok("Welcome To GIT Project");
+	}
+}
